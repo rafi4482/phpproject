3573f97511fb02c3e018301fff3e69cbc2d67b3f
Md Rafi Uz Zaman <rafi.kazla@gmail.com>
2022-12-05 14:51:04 +0600

    f

--- myhome.php
+++ myhome.php
@@ -8,7 +8,7 @@ require "dbconfig/config.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DhakaChaka Car Rental</title>
+    <title>DhakaChaka Caar Rental</title>
     <link rel="stylesheet" href="myhome.css">
 
 </head>
